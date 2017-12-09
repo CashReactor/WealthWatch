6e9894a152f88772fac26685699583bbcd8fd395
Daniel <dkwonism@gmail.com>
2017-12-09 13:04:25 -0500

    Create separate component for InputBalance

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Graph from './components/Graph.jsx'
+import Graph from './components/graph.jsx';
+import InputBalance from './components/inputBalance.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -9,67 +10,13 @@ class App extends React.Component {
       budget: 0,
       budgetInput: false
     }
-    this.budgetToggle = this.budgetToggle.bind(this);
-    this.searchBar = this.searchBar.bind(this);
-  }
-
-  searchBar() {
-    if (this.state.budgetInput) {
-      return (
-        <div>
-          <form action="">
-            Input Balance: <input type="text" name="balance"/><br></br>
-            <select name="currency_code">
-              <option value="">Select Currency</option>
-              <option value="USD">U.S. Dollar</option>
-              <option value="AUD">Australian Dollar</option>
-              <option value="BRL">Brazilian Real </option>
-              <option value="CAD">Canadian Dollar</option>
-              <option value="CZK">Czech Koruna</option>
-              <option value="DKK">Danish Krone</option>
-              <option value="EUR">Euro</option>
-              <option value="HKD">Hong Kong Dollar</option>
-              <option value="HUF">Hungarian Forint </option>
-              <option value="ILS">Israeli New Sheqel</option>
-              <option value="JPY">Japanese Yen</option>
-              <option value="MYR">Malaysian Ringgit</option>
-              <option value="MXN">Mexican Peso</option>
-              <option value="NOK">Norwegian Krone</option>
-              <option value="NZD">New Zealand Dollar</option>
-              <option value="PHP">Philippine Peso</option>
-              <option value="PLN">Polish Zloty</option>
-              <option value="GBP">Pound Sterling</option>
-              <option value="SGD">Singapore Dollar</option>
-              <option value="SEK">Swedish Krona</option>
-              <option value="CHF">Swiss Franc</option>
-              <option value="TWD">Taiwan New Dollar</option>
-              <option value="THB">Thai Baht</option>
-              <option value="TRY">Turkish Lira</option>
-            </select>
-
-          </form>
-        </div>
-      )
-    } else {
-      return (
-        <div>
-          Hello World
-        </div>
-      )
-    }
-
-  }
-
-  budgetToggle() {
-    this.setState({budgetInput: !this.state.budgetInput})
   }
 
   render() {
     return (
       <div>
         <Graph />
-        <button type="button" onClick={this.budgetToggle} className="btn">Balance</button>
-        {this.searchBar()}
+        <InputBalance/>
       </div>
     )
   }
