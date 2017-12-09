9756796684ce1bf582d9915cf14b6c8a9c713d30
Daniel <dkwonism@gmail.com>
2017-12-09 15:32:49 -0500

    Set weather states upon axios response

--- client/src/components/weather.jsx
+++ client/src/components/weather.jsx
@@ -5,16 +5,26 @@ class Weather extends React.Component {
   constructor(props){
     super(props);
     this.state = {
+      state: '',
+      weather: '',
+      temperature: ''
     }
   }
 
   componentDidMount() {
+    var scope = this;
     navigator.geolocation.getCurrentPosition(function(position) {
       var lat = position.coords.latitude;
       var lon = position.coords.longitude;
       var url = `api.openweathermap.org/data/2.5/weather?lat=${lat}&${lon}&APPID=${process.env.WEATHER_API}`;
       axios.get(url).then(function(response) {
-
+        scope.setState(
+          {
+            state: response.name,
+            weather: response.weather.main + ': ' + response.weather.description,
+            temperature: response.main.temp
+          }
+        )
       })
     })
   }
