ef89ab3d937cb6a2bf9aab535bd8e3dad3606303
Aprianto Wiwarsono <awiwarsono@gmail.com>
2017-12-10 07:30:55 +1300

    Updates routing and add passport middleware

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
+//Import your routes here
+// ***********************
+
+
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 const app = express();
 
 app.set('port', process.env.PORT || 1337);
 const port = app.get('port');
 
 app.use(express.static(__dirname + '/../client/public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Use Routes here.....
 
 app.listen(port, () => {
   console.log('Express is listening on port 1337');
