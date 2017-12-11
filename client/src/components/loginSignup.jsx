e7c00a302f61876186c9ddaffb94f402a728d9bb
Daniel <dkwonism@gmail.com>
2017-12-11 17:53:22 -0500

    Format Bootstrap login and signup forms

--- client/src/components/loginSignup.jsx
+++ client/src/components/loginSignup.jsx
@@ -35,16 +35,17 @@ class LoginSignup extends React.Component {
     return (
       <div>
         <form>
-          <div className="form-group">
+          <div className="form-group col-xs-4">
             <label for="inputEmail">Email Address</label>
             <input type="email" className="form-control" id="loginEmail" aria-describedby="emailHelp" placeholder="Enter email"/>
             <small id="emailHelp" className="form-text text-muted">We'll never share your email to anyone else.</small>
           </div>
-          <div className="form-group">
+          <div className="form-group col-xs-4">
             <label for="inputPassword">Password</label>
             <input type="password" className="form-control" id="loginPassword" placeholder="Password"/>
           </div>
         </form>
+        <button type="submit" className="btn btn-primary">Login</button>
       </div>
     )
   }
@@ -53,25 +54,24 @@ class LoginSignup extends React.Component {
     return (
       <div>
         <form>
-          <div className="form-group">
+          <div className="form-group col-xs-4">
             <label for="inputName">Name</label>
             <input type="text" className="form-control" id="signupName" placeholder="Name"/>
           </div>
-          <div className="form-row">
-            <div className="form-group col-md-6">
-              <label for="inputEmail">Email</label>
-              <input type="email" className="form-control" id="signupEmail" placeholder="Email"/>
-              <small id="emailHelp" className="form-text text-muted">This email account will be used for logging in.</small>
-            </div>
-            <div className="form-group col-md-6">
-              <label for="inputPassword">Password</label>
-              <input type="password" className="form-control" id="signupPassword" placeholder="Password"/>
-            </div>
-            <div className="form-group">
-              <label for="inputName">Profile Image</label>
-              <input type="text" className="form-control" id="signupImageUrl" placeholder="Image Url"/>
-            </div>
+          <div className="form-group col-xs-4">
+            <label for="inputEmail">Email</label>
+            <input type="email" className="form-control" id="signupEmail" placeholder="Email"/>
+            <small id="emailHelp" className="form-text text-muted">This email account will be used for logging in.</small>
           </div>
+            <div className="form-group col-xs-8">
+            <label for="inputPassword">Password</label>
+            <input type="password" className="form-control" id="signupPassword" placeholder="Password"/>
+          </div>
+          <div className="form-group col-xs-8">
+            <label for="inputName">Profile Image</label>
+            <input type="text" className="form-control" id="signupImageUrl" placeholder="Image Url"/>
+          </div>
+          <button type="submit" className="btn btn-primary">Sign Up</button>
         </form>
       </div>
     )
@@ -80,7 +80,7 @@ class LoginSignup extends React.Component {
   render() {
     return (
       <div>
-        {this.loginForm()}
+        {this.loginForm()}<br></br><br></br><br></br><br></br>
         {this.signupForm()}
       </div>
     )
