ec448d3c834a2aa06eb597266a694d72f0b263fd
Jacob <jphill0808@gmail.com>
2017-12-12 20:08:41 -0500

    create current component for hosting subcomponents

---
+++ client/src/components/expenses.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import OneExpense from './oneExpense.jsx';
+import RecExpense from './recExpense.jsx';
+import Paper from 'material-ui/Paper';
+
+const style = {
+  paper: {
+    height: 250,
+    width: 500,
+    textAlign: "center",
+    marginLeft: 400
+  },
+  button: {
+    marginLeft: 400
+  }
+}
+
+class Expenses extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        <Paper style={style.paper} zDepth={5}>
+          Add Expense
+          <OneExpense />
+          <RecExpense />
+        </Paper>
+      </div>
+    )
+  }
+}
+
+export default Expenses;
