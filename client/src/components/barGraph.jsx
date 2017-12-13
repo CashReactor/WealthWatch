2791a17ba824b98368a6e5e5ebf54ade7b94f324
Jacob <jphill0808@gmail.com>
2017-12-13 01:24:50 -0500

    restructure and refactor the bar graph

--- client/src/components/barGraph.jsx
+++ client/src/components/barGraph.jsx
@@ -46,13 +46,8 @@ class BarGraph extends React.Component {
       -250,
       -800,
     ];
-    function randomColor() {
-      let o = Math.round,
-        r = Math.random,
-        s = 230;
-      return 'rgba(' + o(r() * s) + ',' + o(r() * s) + ',' + o(r() * s) + ',' + r().toFixed(1) + ')';
-    }
-    let randomColors = updatedBudgets.map(budget => (budget > 0 ? randomColor() : 'rgba(255, 0, 0, 0.5)'));
+    let positiveColor = 'rgba(0,0,210, 0.5)'
+    let color = updatedBudgets.map(budget => (budget > 0 ? positiveColor : 'rgba(255, 0, 0, 0.5)'));
     var barGraph = new Chart(barCtx, {
       type: 'bar',
       data: {
@@ -61,8 +56,8 @@ class BarGraph extends React.Component {
           {
             label: 'Current Monthly Balance ($)',
             data: updatedBudgets,
-            backgroundColor: randomColors,
-            borderColor: randomColors,
+            backgroundColor: color,
+            borderColor: color,
             borderWidth: 1,
           },
         ],
