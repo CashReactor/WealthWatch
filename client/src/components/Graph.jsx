cecdc9df1f6a7d1da4ba10dfeb1de4b7df4394b2
Jacob <jphill0808@gmail.com>
2017-12-13 01:23:43 -0500

    wrap components with paper component

--- client/src/components/Graph.jsx
+++ client/src/components/Graph.jsx
@@ -2,6 +2,21 @@ import React from 'react';
 import { Tabs, Tab } from 'material-ui/Tabs';
 import BarGraph from './barGraph.jsx';
 import LineGraph from './lineGraph.jsx';
+import Paper from 'material-ui/Paper';
+
+var styles = {
+  default_tab:{
+    color: 'rgba(255, 255, 255, 0.9)',
+    backgroundColor: 'rgba(40, 40, 227, 0.8)',
+    fontWeight: 400,
+  },
+  paper: {
+    height: 750,
+    width: 1220,
+    textAlign: "center",
+    marginLeft: 5,
+  }
+}
 
 class Graph extends React.Component {
   constructor(props) {
@@ -15,16 +30,18 @@ class Graph extends React.Component {
   render() {
     return (
       <div>
-        <Tabs>
-          <Tab label="Bar Graph">
-            <div>
-              <BarGraph />
-            </div>
-          </Tab>
-          <Tab label="Line Graph">
-            <LineGraph />
-          </Tab>
-        </Tabs>
+        <Paper style={styles.paper} zDepth={5}>
+          <Tabs>
+            <Tab style={styles.default_tab} label="Bar Graph">
+              <div>
+                <BarGraph />
+              </div>
+            </Tab>
+            <Tab style={styles.default_tab} label="Line Graph">
+              <LineGraph />
+            </Tab>
+          </Tabs>
+        </Paper>
       </div>
     );
   }
