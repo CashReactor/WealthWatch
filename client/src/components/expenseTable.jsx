dc31b363f73be2a134f4373a7f4c46ae455faec3
Michael MacLean <mrmaclean89@gmail.com>
2017-12-15 16:27:00 -0800

    Set up Material-UI/table for expenseTable component

---
+++ client/src/components/expenseTable.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import axios from 'axios';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn
+} from 'material-ui/Table';
+
+
+class ExpenseTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+
+    return (
+      <div>
+
+      )
+
+  }
+
+
+}
