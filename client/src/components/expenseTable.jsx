c0c2aa211a8c11694e06f5241dbc8c7880918236
Michael MacLean <mrmaclean89@gmail.com>
2017-12-15 19:20:00 -0800

    Insert two separate expense tables, one-time and recurring, using material-ui/table. Troubleshoot rendering: not passing expense objects correctly.

--- client/src/components/expenseTable.jsx
+++ client/src/components/expenseTable.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import axios from 'axios';
+import Paper from 'material-ui/Paper';
 import {
   Table,
   TableBody,
@@ -8,6 +8,7 @@ import {
   TableRow,
   TableRowColumn
 } from 'material-ui/Table';
+
 
 
 class ExpenseTable extends React.Component {
@@ -20,7 +21,66 @@ class ExpenseTable extends React.Component {
   render() {
 
     return (
+
       <div>
+
+        <div>
+          <Table rec={this.props.rec}>
+            <TableHeader>
+              <TableRow>
+                <TableHeaderColumn colSpan="3" tooltip="Super Header" style={{textAlign: 'center'}}>
+                  Recurring Expenses
+                </TableHeaderColumn>
+              </TableRow>
+              <TableRow>
+                <TableRowColumn>Date</TableRowColumn>
+                <TableRowColumn>Recurring Expense</TableRowColumn>
+                <TableRowColumn>Category</TableRowColumn>
+                <TableRowColumn>Amount</TableRowColumn>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+            {this.props.rec.map( (expense) => (
+              <TableRow>
+                <TableRowColumn>{expense.date}</TableRowColumn>
+                <TableRowColumn>{expense.expense}</TableRowColumn>
+                <TableRowColumn>{expense.category}</TableRowColumn>
+                <TableRowColumn>{expense.amount}</TableRowColumn>
+              </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+
+        <div>
+          <Table one={this.props.one}>
+            <TableHeader>
+              <TableRow>
+                <TableHeaderColumn colSpan="3" tooltip="Super Header" style={{textAlign: 'center'}}>
+                  Non-Recurring Expenses
+                </TableHeaderColumn>
+              </TableRow>
+              <TableRow>
+                <TableRowColumn>Date</TableRowColumn>
+                <TableRowColumn>Expense</TableRowColumn>
+                <TableRowColumn>Category</TableRowColumn>
+                <TableRowColumn>Amount</TableRowColumn>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+            {this.props.one.map( (expense) => (
+              <TableRow>
+                <TableRowColumn>{expense.date}</TableRowColumn>
+                <TableRowColumn>{expense.expense}</TableRowColumn>
+                <TableRowColumn>{expense.category}</TableRowColumn>
+                <TableRowColumn>{expense.amount}</TableRowColumn>
+              </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+
+      </div>
 
       )
 
@@ -28,3 +88,6 @@ class ExpenseTable extends React.Component {
 
 
 }
+
+
+export default ExpenseTable;
