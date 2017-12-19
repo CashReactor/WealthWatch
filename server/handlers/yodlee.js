4738aa89497eae93f439045dc443f8ffb53d9b2e
Michael MacLean <mrmaclean89@gmail.com>
2017-12-18 19:49:38 -0800

    Create Yodlee Transaction handler, require plugins understood to be necessary.

---
+++ server/handlers/yodlee.js
@@ -0,0 +1,10 @@
+var yTrans = require('yodlee-transactions');
+var yodlee = require("./index");
+var cobrandUser = ""; //example format in sbCobxxxxx
+var cobrandPassword = ""; //format is xxxx-xxx-xxx-xxx-xxxx the number of x is not accurate
+var userName = ""; //example formats are sbMemxxxxx
+var userPassword = ""; //example formate are in sbMemxxxxx
+var cobSessionToken = "";
+var userSessionToken = "";
+var accounts = [];
+
