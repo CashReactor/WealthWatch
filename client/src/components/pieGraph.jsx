e07330e7a324db0dfb2989056a64c89eb9edc94e
Jacob <jphill0808@gmail.com>
2017-12-20 14:26:42 -0500

    update

---
+++ client/src/components/pieGraph.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class PieGraph extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+    }
+  }
+  render() {
+    <div>
+      <canvas id='pieChart'/>
+    </div>
+  }
+}
+
+export default PieGraph;
