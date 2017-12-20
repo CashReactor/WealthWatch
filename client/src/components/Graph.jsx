1d13c180e3a524264d3899673946ff43733febc5
Jacob <jphill0808@gmail.com>
2017-12-20 14:46:33 -0500

    fix render issue

--- client/src/components/Graph.jsx
+++ client/src/components/Graph.jsx
@@ -67,6 +67,7 @@ class Graph extends React.Component {
         {/*<Paper style={styles.paper}>
           <BarGraph currentEmail={this.props.currentEmail} budget={this.props.budget} one={this.props.one} rec={this.props.rec} />
           <LineGraph currentEmail={this.props.currentEmail} one={this.props.one} rec={this.props.rec} />
+<<<<<<< 44e5cf9165893bd6020d5a839ecc790c61af91d9
 <<<<<<< 65cdde2d146e6a38e87e423184c30b0496d0c24b
         </Paper>*/}
       </div>
