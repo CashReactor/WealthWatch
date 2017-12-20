0252f0c5f2fb80edeac3b907d374879861a79441
Jacob <jphill0808@gmail.com>
2017-12-20 12:48:14 -0500

    add pieGraph component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,5 +3,4 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import App from './components/app.jsx';
 
-
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('app'));
